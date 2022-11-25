c5763d7b7a55bda6f2ef3fba6d43046ed07ad1d2
RadeoalaPaul <44653419+RadeoalaPaul@users.noreply.github.com>
2022-11-25 12:03:41 +0200

    v0.2

--- Hangman/Hangman.cpp
+++ Hangman/Hangman.cpp
@@ -3,128 +3,99 @@
 #include <string>
 #include <windows.h>
 #include <time.h>
+#include <list>
 
 using namespace std;
 
-fstream f("cuvinte.in");
+fstream f;
 
-void choice(int choice);
-char folosita[33];
+void mainMenu();
+void threeDots();
+
+bool verificaCuvantExistent(string cuvant) {
+	string cuvinte[120], c;
+	bool existent = false;
+	f.open("cuvinte.in");
+	while (getline(f, c)) {
+		if (c == cuvant) existent = true;
+	}
+	f.close();
+	if (existent) return true;
+	else return false;
+}
+
+void adaugaCuvant() {
+	string cuvant;
+	system("cls");
+	cout << "~~~~~HANGMAN~~~~~\n\nCuvantul de adaugat: "; cin >> cuvant;
+	while (verificaCuvantExistent(cuvant)) {
+		system("cls");
+		cout << "~~~~~HANGMAN~~~~~\n\nAcest cuvant exista in baza de date!\n\nCuvantul de adaugat: "; cin >> cuvant;
+	}
+	f.open("cuvinte.in", ios::app);
+	f << cuvant << endl;
+	f.close();
+	cout << "\nCuvantul a fost adaugat cu succes, vei fi redirectionat catre meniul principal.\n\nSe incarca";
+	threeDots();
+	mainMenu();
+}
+
+void Iesire() {
+	system("cls");
+	cout << "~~~~~HANGMAN~~~~~\n\nJocul se va inchide in catea secunde.\n\nSe incarca";
+	threeDots();
+	exit(0);
+}
+
+void choice(int choice) {
+	switch (choice) {
+	case 1: {
+		list<string> cuvinte;
+		string s;
+		f.open("cuvinte.txt");
+		int i = 0;
+		while (getline(f, s)) {
+			cuvinte.push_back(s);
+		}
+		for (string v : cuvinte) {
+			cout << v << endl;
+		}
+		f.close();
+		break;
+	}
+	case 2: {
+		adaugaCuvant();
+		break;
+	}
+	case 3: {
+		Iesire();
+		break;
+	}
+	default: {
+		break;
+	}
+	}
+}
 
 void mainMenu() {
 	system("cls");
 	cout << "~~~~~HANGMAN~~~~~";
 	int alegere;
-	cout << "~~~~~HANGMAN~~~~~\n\nAlege din meniul de mai jos\n\n1.Intra in joc\n2.Iesi din joc\n\nAlegere: ";
+	cout << "~~~~~HANGMAN~~~~~\n\nAlege din meniul de mai jos\n\n1.Intra in joc\n2.Adauga cuvant\n3.Iesi din joc\n\nAlegere: ";
 	cin >> alegere;
 	choice(alegere);
 }
+
 void threeDots() {
 	for (int i = 0; i < 3; i++) {
 		cout << ".";
 		Sleep(1000);
 	}
 }
-bool analiza(string cuvant, char litera) {
-	int i = 0;
-	for (i; i < cuvant.length(); i++) {
-		if (cuvant[i] == litera) {
-			return true;
-		}
-	}
-	return false;
-}
-void choice(int choice) {
-	switch (choice) {
-	case 1: {
-		int i = 0, j = 0, random, sanse = 5;
-		string cuvinte[120], cuvant, cuvant_de_ghicit, cuvant_ghicit[20];
-		char litera, litera_folosita[33];
-		system("cls");
-		cout << "~~~~~HANGMAN~~~~~\n\nUn cuvant a fost ales.\nTrebuie sa ii ghicesti pe rand literele.\n\n";
-		while (getline(f, cuvant)) {
-			cuvinte[i] = cuvant;
-			i++;
-		}
-		random = rand() % i;
-		cuvant_de_ghicit = cuvinte[random];
-		for (int i = 0; i < cuvant_de_ghicit.length(); i++) {
-			cout << "_ ";
-			cuvant_ghicit[i] = "_";
-		}
-		cout << "\n\nAi " << sanse << " sanse\n\n" << "Litera: ";
-		cin >> litera;
-		while (sanse != 0) {
-			if (analiza(cuvant_de_ghicit, litera)) {
-				string cuvant_final;
-				int k = 0;
-				for (int i = 0; i < cuvant_de_ghicit.length(); i++) {
-
-				}
-				for (int i = 0; i < cuvant_de_ghicit.length(); i++) {
-					//k++;
-					//if (cuvant_de_ghicit[i] == cuvant_final[i]) {
-						if (k == cuvant_de_ghicit.length() - 1) {
-							cout << "Felicitari, ai castigat! Vei fi redirectionat la meniul initial.\n\nSe incarca";
-							threeDots();
-						}
-					//}
-					else break;
-				}
-				system("cls");
-				cout << "~~~~~HANGMAN~~~~~\n\n";
-				litera_folosita[j] = litera;
-				j++;
-				cout << "Litere folosite: ";
-				/*if (cuvant_de_ghicit == cuvant_ghicit) {
-					cout << "Felicitari ai castigat!";
-				}
-				for (int i = 0; i < cuvant_ghicit.length(); i++) {
-				
-				}*/
-
-				for (int i = 0; i < cuvant_de_ghicit.length(); i++) {
-					
-				}
-				for (int i = 0; i < cuvant_de_ghicit.length(); i++) {
-					if (cuvant_de_ghicit[i] == litera) cuvant_ghicit[i] = litera;
-				}
-				cout << "\n\n";
-				for (int i = 0; i < cuvant_de_ghicit.length(); i++) {
-					cout << cuvant_ghicit[i] << " ";
-				}
-				cout << "\n\nAi " << sanse << " sanse\n\n" << "Litera: ";
-				cin >> litera;
-				k = cuvant_de_ghicit.length() - 1;
-			}
-		}
-		break;
-	}
-	case 2: {
-		system("cls");
-		cout << "~~~~~HANGMAN~~~~~\n\nAplicatia se va inchide imediat\n\nSe incarca";
-		threeDots();
-		exit(0);
-	}
-		default: {
-			break;
-		}
-	}
-}
 
 int main() {
 	srand(time(NULL));
+	f.open("cuvinte.txt");
 	mainMenu();
-	string cuvant, cuvinte[10];
-	int i = 0;
-	while (getline(f, cuvant)) {
-		cuvinte[i] = cuvant;
-		i++;
-	}
-	for (int i = 0; i < 2; i++) {
-		cout << cuvinte[i] << " ";
-	}
-	char cacat[10];
-	cout << cuvinte[0];
 	return 0;
 }
